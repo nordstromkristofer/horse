e2d850614de681fdc87935cb2d738529f0596f0d
nordstromkristofer <nordstromkristofer@gmail.com>
2022-05-16 13:52:33 +0200

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>  
+        <a className='text-left' href="/">Hem</a><br></br>
+        <a className='text-left' href="/product-list">Produktlista</a><br></br>
+        <a className='text-left' href="/shopping-cart">Varukorg</a><br></br>
+        
+        <br></br> <br></br>
+  </div>
+  )
+}
+
+export default Header
