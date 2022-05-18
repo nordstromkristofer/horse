834a420ab44611e34170ba0172081da21828911a
Kim Michael Garcia Haas <Kgh13690@gmail.com>
2022-05-18 23:06:24 +0200

    Changed in database in category. Diffrent horse category

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "../scss/style.scss";
 import App from './App';
-import Header from './Header'
+import Header from './Header';
+
+
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
