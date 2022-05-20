e5f12aa785bf0ae6d41eadd359411d6929f93da4
Markus Bellika <markusbellika@gmail.com>
2022-05-20 16:31:11 +0200

    trying to fix input form

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,21 +1,30 @@
 import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
-const Create = () => (
-  <div>
-    <Form>
-      <label>Namn</label>
-      <input placeholder='Namn' />
+export default function Create() {
 
-      <label>Beskrivning</label>
-      <input placeholder='Beskrivning' />
+  const [horseName, setHorseName] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
 
-      <label>Pris</label>
-      <input placeholder='Pris' />
+  const postData = () => {
+    console.log(horseName)
+  }
 
-      <Button type='submit'>OK</Button>
-    </Form>
-  </div>
-)
+  return (
+    <div>
+      <Form>
+        <label>Namn</label>
+        <input placeholder='Namn' onChange={(e) => setHorseName(e.target.value)} />
 
-export default Create
+        <label>Beskrivning</label>
+        <input placeholder='Beskrivning' />
+
+        <label>Pris</label>
+        <input placeholder='Pris' />
+
+        <Button onClick={postData} type='submit'>OK</Button>
+      </Form>
+    </div>
+  )
+}
