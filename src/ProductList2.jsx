3ec5a8bb8f3f215c899cd08b63a0440812e6cbb8
nordstromkristofer <nordstromkristofer@gmail.com>
2022-05-20 12:03:08 +0200

    Filstruktur och productlist2, utan klutter.

---
+++ src/ProductList2.jsx
@@ -0,0 +1,34 @@
+import { useStates } from './utilities/states';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
+import { scrollRestore } from './utilities/scrollBehavior';
+import CategorySelect from './CategorySelect';
+
+
+export default function ProductList2() {
+
+  scrollRestore();
+
+  let s = useStates('main');
+  let navigate = useNavigate();
+
+  function showDetail(id) {
+    navigate(`/product-detail/${id}`);
+  }
+
+  return <Container className="productList">
+    <Row className="mb-3"><Col><CategorySelect showAllOption bindTo={[s, 'chosenCategoryId']} /></Col></Row>
+    {s.products.filter(product =>
+      s.chosenCategoryId === 0 /*all*/
+      || s.chosenCategoryId === product.categoryId
+    ).map(({ id, name, }) =>
+      <Row className="product" key={id} onClick={() => showDetail(id)}>
+        <Card>
+          <Col xxl="12">
+            <h3>{name}</h3>
+          </Col>
+        </Card>
+      </Row>
+    )}
+  </Container>
+}
