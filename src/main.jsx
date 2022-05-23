a7ab023009439b01a0e3c93246221bdd8a79542c
Mo Arefi <m-arefi@live.com>
2022-05-23 11:51:52 +0200

    Just in case

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "../scss/style.scss";
 import App from './App';
-<<<<<<< HEAD
-import Header from './Header'
-import Navbar from './Navbar'
-=======
 import Header from './Header';
-
-
->>>>>>> origin/dev
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
