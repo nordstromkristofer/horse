abd3d99ea843bbd1449dc80959b495662e1999f5
Mo Arefi <m-arefi@live.com>
2022-05-24 09:07:37 +0200

    Nav Pushed

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import "../scss/style.scss";
 import App from './App';
 import Header from './Header';
+import Navbar from './Navbar';
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
