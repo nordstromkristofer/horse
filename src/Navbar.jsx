9cc28727b9992cc80eaf4f8a8fd505dfc40fb7b9
Mo Arefi <m-arefi@live.com>
2022-05-24 09:38:00 +0200

    Search added to Navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,12 +6,24 @@ import { Link } from 'react-scroll';
 const Navbar = () => {
 const [nav, setNav] = useState(false);
 const handleClick = () => setNav(!nav);
-
+function handleChange() {
+  console.log(handleChange);
+}
+    
 return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
     <div>
         <img src={Logo} alt='Logo Image' style={{ width: '50px' }} />
-    </div>
+        </div>
+        
+        {/* Search Bar */}
+
+        <div className='flex justify-end'>
+            <input
+                placeholder="Golden Horse 🔍"
+                onChange={handleChange} />
+        </div>
+
 
       {/* menu */}
     
