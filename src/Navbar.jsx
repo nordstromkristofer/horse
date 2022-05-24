44b670b58d8c1e47b5cacde91570c78b0623eeb5
Mo Arefi <m-arefi@live.com>
2022-05-24 11:24:55 +0200

    Navbar Out of the way

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -46,21 +46,21 @@ return (
         </li>
         </ul>
         
-                {/* Search Bar */}
+        {/* Search Bar */}
 
         <form class="rounded-md items-center flex divide-x">
-          <select class="rounded-l-md  py-4">
+        <select class="rounded-l-md  py-4">
             <option selected="selected">All Category</option>
             <option>Horses</option>
             <option>Equipment</option>
             <option>Edibles</option>
-          </select>
-          <input class="py-3.5 px-2 appearance-none block w-full  focus:outline-none focus:bg-green-50 placeholder-gray-500 placeholder-opacity-25" name="search" placeholder="Search Products ....." type="search"/>
-          <button class="py-3 px-2 bg-yellow-400">
+        </select>
+        <input class="py-3.5 px-2 appearance-none block w-full  focus:outline-none focus:bg-green-50 placeholder-gray-500 placeholder-opacity-25" name="search" placeholder="Search Products ....." type="search"/>
+        <button class="py-3 px-2 bg-yellow-400">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-8 text-gray-50 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
-          </button>
+        </button>
         </form>
 
       {/* Hamburger */}
