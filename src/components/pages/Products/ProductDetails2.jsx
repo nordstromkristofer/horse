877c7eca5936e226a3d068545bce775fd86561d8
Markus Bellika <markusbellika@gmail.com>
2022-05-30 12:30:07 +0200

    försöker fixa backend crud :(

--- src/components/pages/Products/ProductDetails2.jsx
+++ src/components/pages/Products/ProductDetails2.jsx
@@ -1,11 +1,6 @@
-import { useStates } from "../../../utilities/states";
-import { Container, Row, Col, Card } from "react-bootstrap";
-import { useParams, Link, useNavigate } from "react-router-dom";
-import { add } from "../../../utilities/shoppingCartLogic";
-import { sweFormat } from "../../../utilities/currencyFormatter";
-
-export default function ProductDetail1() {
+export function ProductDetail2() {
   let s = useStates("main");
+  let navigate = useNavigate();
 
   // Find the product
   let { id } = useParams();
@@ -20,20 +15,18 @@ export default function ProductDetail1() {
   let categoryName =
     s.categories.find((category) => category.id === categoryId)?.name || "none";
 
-  let navigate = useNavigate();
-
-  function buy() {
-    // Add the product to the cart
-    add(product);
-    // Show the cart
-    navigate("/shopping-cart");
+  async function deleteBtn() {
+    // Save to db
+    await product.delete();
+    // Navigate to detail page
+    navigate(`/Backoffice/`);
   }
 
   return (
     <Container className="productList">
       <Row>
         <Col>
-          <Link to={`/`}>
+          <Link to={`/Backoffice`}>
             <button type="button" className="my-4 btn btn-primary">
               Back to list
             </button>
@@ -58,26 +51,30 @@ export default function ProductDetail1() {
       </Row>
       <Row>
         <Col>
-          <p>Price: {sweFormat(price)} </p>
+          <p>Price: {price} SEK</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Link to={`/product-edit/${id}`}>
+            <button type="button" className="my-4 btn btn-primary float-end">
+              Edit
+            </button>
+          </Link>
+          <Link to={`/product-edit/${id}`}>
+            <Button
+              variant="danger"
+              type="button"
+              onClick={deleteBtn}
+              className="my-4 btn btn-primary float-left"
+            >
+              Delete
+            </Button>
+          </Link>
         </Col>
       </Row>
       <Row>
         <Col></Col>
-      </Row>
-      <Row>
-        <Col>
-          <label>
-            Antal
-            <input type="text" name="antal" placeholder="1" />
-            <button
-              type="button"
-              onClick={buy}
-              className="mt-2 btn btn-primary float-end"
-            >
-              Buy
-            </button>
-          </label>
-        </Col>
       </Row>
     </Container>
   );
