991bc9ba56ef91c859e82b32687d0cf8d30336fa
Sara Razm <razmsara@gmail.com>
2022-05-30 22:57:55 +0200

    Footer added

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import "../scss/style.scss";
 import App from "./App";
 import "./index.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 ReactDOM.createRoot(document.querySelector("#root")).render(
   <React.StrictMode>
     <Navbar />
     <App />
+    <Footer />
   </React.StrictMode>
 );
