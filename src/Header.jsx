07c83e63f83ff045b9052b1d5321ee5dfe3f5119
Markus Bellika <markusbellika@gmail.com>
2022-05-30 11:46:56 +0200

    fix

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,48 +1,32 @@
-import React from 'react';
-
-
+import React from "react";
 
 function handleChange() {
-<<<<<<< HEAD
   console.log(handleChange);
 }
-=======
-  console.log(handleChange); 
-      }
->>>>>>> 6d50c47bcb78109e2822d4b41da914348dad5ac5
 const Header = () => {
-
-
-
-
   return (
-    <div className='header'>
-      <a className='text-left' href="/">Stallet</a><br></br>
-      <a className='text-left' href="/product-list">Hästar</a><br></br>
-      <a className='text-left' href="/shopping-cart">Varukorg</a><br></br>
-
+    <div className="header">
+      <a className="text-left" href="/">
+        Stallet
+      </a>
+      <br></br>
+      <a className="text-left" href="/product-list">
+        Hästar
+      </a>
+      <br></br>
+      <a className="text-left" href="/shopping-cart">
+        Varukorg
+      </a>
+      <br></br>
       <br></br> <br></br>
-
-
-
       {
         //Search bar on navbar
         <div>
-          <input
-            placeholder="Golden Horse 🔍"
-<<<<<<< HEAD
-            onChange={handleChange} />
+          <input placeholder="Golden Horse 🔍" onChange={handleChange} />
         </div>
       }
     </div>
-=======
-           
-            onChange={handleChange}  />
-          </div>
-        }
-  </div>
->>>>>>> 6d50c47bcb78109e2822d4b41da914348dad5ac5
-  )
-}
+  );
+};
 
 export default Header;
