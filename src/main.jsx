14fe9f104efbe89b090a4e9ecf6facef2f5f45ba
Mohammad Arefi <m-arefi@live.com>
2022-05-31 11:57:36 +0200

    TestimonialJSX Init

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import App from "./App";
 import "./index.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Testimonials from "./components/Testimonials";
 
 ReactDOM.createRoot(document.querySelector("#root")).render(
   <React.StrictMode>
     <Navbar />
     <App />
+    <Testimonials />
     <Footer />
   </React.StrictMode>
 );
