d3f2d867efd47add970bdb48ec18772d18a4be1c
Mo Arefi <m-arefi@live.com>
2022-06-02 12:03:25 +0200

    CreateJSX Dropdown

--- src/components/pages/Backoffice/Edit/Create.jsx
+++ src/components/pages/Backoffice/Edit/Create.jsx
@@ -71,7 +71,15 @@ const Create = () => {
         />
         <Row>
           <select
-            className="catSelect"
+            className="dropdown-toggle
+          w-3/4 px-6 py-2.5 mb-2 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg
+          transition
+          duration-150
+          ease-in-out
+          flex
+          items-center
+          whitespace-nowrap
+          text-center"
             onChange={(e) => {
               const selectedCat = e.target.value;
               setCatState(selectedCat);
