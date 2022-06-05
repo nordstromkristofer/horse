2ab3b13d78dfd5c2b026e1a0e478aabca37f6291
Sara Razm <razmsara@gmail.com>
2022-06-05 21:35:09 +0200

    Product Edit and camera funcation's bug has fixed

--- src/components/pages/Products/ProductEdit.jsx
+++ src/components/pages/Products/ProductEdit.jsx
@@ -49,89 +49,98 @@ export default function ProductDetail() {
   }
 
   return (
-    <form className="block p-6 rounded-lg shadow-lg bg-white">
-      <div className="block bg-white">
-        <div className="border rounded-md px-6 py-10 max-w-2xl mx-auto">
-          <h1 className="text-center text-2xl font-bold text-orange-600 my-8">
-            Redigera produktinformation
-          </h1>
-          <div className="flex-col">
-            <div className="space-y-2 my-2">
-              <div>
-                <div>
-                  <h1 className="uppercase text-center">{name}</h1>
-                </div>
-              </div>
-              <div>
-                <div className="text-justify">
-                  <p>{description}</p>
-                </div>
-              </div>
-              <div>
-                <div>
-                  <p>Pris: {price} SEK</p>
-                </div>
-              </div>
-            </div>
-            <label className="">
-              Redigera namnet:
-              <input
-                className="form-control my-2 border-2 w-full px-4 py-2 rounded-md text-md text-gray-700 outline-none"
-                {...product.bind("name")}
-              />
-            </label>
-            <label className="">
-              Redigera beskrivningen:
-              <textarea
-                rows={4}
-                className="form-control my-2 resize-none border-2 w-full px-4 py-2 rounded-md text-md text-gray-700 outline-none"
-                {...product.bind("description")}
-              />
-            </label>
-            <label className="my-2">
-              Redigera priset:
-              <input
-                className="form-control my-2 border-2 w-full px-4 py-2 rounded-md text-md text-gray-700 outline-none"
-                {...product.bind("price")}
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              Kategori:&nbsp;
-              <CategorySelect bindTo={[product, "categoryId"]} />
-            </label>
-          </div>
-          <div className="my-2 border-2 w-full px-4 py-2 rounded-md text-md text-gray-700">
-            <div className="">
-              <video
-                style={{ display: l.captureMode ? "block" : "none" }}
-                autoPlay
-              ></video>
-              <canvas
-                width="320px"
-                height="240px"
-                style={{ display: !l.captureMode ? "block" : "none" }}
-              ></canvas>
-              <button
-                onClick={takeImage}
-                className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-              >
-                Ta Bild
-              </button>
-            </div>
-            <div>
-              <input
-                type="file"
-                className="my-4  inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                onChange={imagePicker}
-              />
-            </div>
-          </div>
-          <div className="mt-4"></div>
+    <Container className="border rounded-md px-6 py-10 max-w-2xl mx-auto">
+      <Row>
+        <Col>
+          <video
+            style={{ display: l.captureMode ? "block" : "none" }}
+            autoPlay
+          ></video>
+          <canvas
+            width="320px"
+            height="240px"
+            style={{ display: !l.captureMode ? "block" : "none" }}
+          ></canvas>
           <button
-            onClick={save}
-            className="w-full
+            onClick={takeImage}
+            className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+          >
+            Ta Bild
+          </button>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h1>Välj bild</h1>
+          <input
+            type="file"
+            className="my-4  inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            onChange={imagePicker}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h1 className="uppercase text-center">{name}</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div className="text-justify">
+            <p>{description}</p>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p>Pris: {price} SEK</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <label className="">
+            Redigera namnet:
+            <input
+              className="form-control my-2 border-2 w-full px-4 py-2 rounded-md text-md text-gray-700 outline-none"
+              {...product.bind("name")}
+            />
+          </label>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <label className="">
+            Redigera beskrivningen:
+            <textarea
+              rows={4}
+              className="form-control my-2 resize-none border-2 w-full px-4 py-2 rounded-md text-md text-gray-700 outline-none"
+              {...product.bind("description")}
+            />
+          </label>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <label className="my-2">
+            Redigera priset:
+            <input
+              className="form-control my-2 border-2 w-full px-4 py-2 rounded-md text-md text-gray-700 outline-none"
+              {...product.bind("price")}
+            />
+          </label>
+        </Col>
+      </Row>
+      <Row className="mt-4">
+        <Col>
+          <label>
+            Kategori:&nbsp;
+            <CategorySelect bindTo={[product, "categoryId"]} />
+          </label>
+        </Col>
+      </Row>
+      <button
+        onClick={save}
+        className="w-full
       px-6
       py-2.5
       bg-blue-600
@@ -148,11 +157,9 @@ export default function ProductDetail() {
       transition
       duration-150
       ease-in-out"
-          >
-            Spara
-          </button>
-        </div>
-      </div>
-    </form>
+      >
+        Spara
+      </button>
+    </Container>
   );
 }
